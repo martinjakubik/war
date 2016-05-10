2d8cd5b4084fba755582c3ec635b1ba3782e8874
martin jakubik <martin.jakubik@gmail.com>
2016-05-10 21:31:40 +0200

    some jslint

--- app.js
+++ app.js
@@ -1,220 +1,219 @@
-var setup = function () {
-  document.open();
-}
+(function () {
 
-var close = function () {
-  document.close();
-}
+    var setup = function () {
+        document.open();
+    };
 
-var renderPlayerTable = function (nPlayer, aPlayerTable) {
+    var close = function () {
+        document.close();
+    };
 
-  var oPlayerTableView = document.getElementById('table' + nPlayer);
+    var renderPlayerTable = function (nPlayer, aPlayerTable) {
 
-  while (oPlayerTableView.firstChild) {
-    oPlayerTableView.removeChild(oPlayerTableView.firstChild);
-  }
+        var i, oPlayerTableView = document.getElementById('table' + nPlayer);
 
-  for (i = 0; i < aPlayerTable.length; i++) {
-    addCardToView(oPlayerTableView, aPlayerTable[i]);
-  }
-
-}
-
-var renderPlayerHand = function (nPlayer, aPlayerCards) {
-
-  var oPlayerHandView = document.getElementById('hand' + nPlayer);
-
-  while (oPlayerHandView.firstChild) {
-    oPlayerHandView.removeChild(oPlayerHandView.firstChild);
-  }
-
-  for (i = 0; i < aPlayerCards.length; i++) {
-    addCardToView(oPlayerHandView, aPlayerCards[i]);
-  }
-
-}
-
-var putCardOnTable = function (aPlayerTable, aPlayerCards) {
-  aPlayerTable.push(aPlayerCards[0]);
-  aPlayerCards.splice(0, 1);
-}
-
-var clearTable = function (aPlayerTable) {
-  aPlayerTable.splice(0);
-}
-
-var getTableCard = function (aPlayerTable) {
-  return aPlayerTable[aPlayerTable.length - 1];
-}
-
-var isGameFinished = function (aPlayer1Cards, aPlayer2Cards) {
-  if (aPlayer1Cards.length === 0) {
-      console.log('player 2 wins');
-      return true;
-  } else if (aPlayer2Cards.length === 0) {
-      console.log('player 1 wins');
-      return true;
-  }
-  return false;
-}
-
-var makeView = function (aPlayer1Cards, aPlayer2Cards) {
-
-  var nPlayState = 0;
-  var aPlayer1Table = [];
-  var aPlayer2Table = [];
-
-  var PLAY_STATE = {
-    movingToTable: 0,
-    checkingTable: 1
-  }
-
-  var doTurn = function (aPlayer1Cards, aPlayer2Cards) {
-
-    switch (nPlayState) {
-      case PLAY_STATE.movingToTable:
-
-        if (isGameFinished(aPlayer1Cards, aPlayer2Cards)) {
-          return;
+        while (oPlayerTableView.firstChild) {
+            oPlayerTableView.removeChild(oPlayerTableView.firstChild);
         }
 
-        putCardOnTable(aPlayer1Table, aPlayer1Cards);
-        putCardOnTable(aPlayer2Table, aPlayer2Cards);
-
-        nPlayState = PLAY_STATE.checkingTable;
-
-        break;
-
-      case PLAY_STATE.checkingTable:
-
-        if (getTableCard(aPlayer1Table) > getTableCard(aPlayer2Table)) {
-          Array.prototype.push.apply(aPlayer1Cards, aPlayer1Table);
-          Array.prototype.push.apply(aPlayer1Cards, aPlayer2Table);
-          clearTable(aPlayer1Table);
-          clearTable(aPlayer2Table);
-        } else if (getTableCard(aPlayer1Table) < getTableCard(aPlayer2Table)) {
-          Array.prototype.push.apply(aPlayer2Cards, aPlayer1Table);
-          Array.prototype.push.apply(aPlayer2Cards, aPlayer2Table);
-          clearTable(aPlayer1Table);
-          clearTable(aPlayer2Table);
-        } else if (getTableCard(aPlayer1Table) === getTableCard(aPlayer2Table)) {
-          putCardOnTable(aPlayer1Table, aPlayer1Cards);
-          putCardOnTable(aPlayer2Table, aPlayer2Cards);
-
+        for (i = 0; i < aPlayerTable.length; i++) {
+            addCardToView(oPlayerTableView, aPlayerTable[i]);
         }
 
-        isGameFinished(aPlayer1Cards, aPlayer2Cards);
-        nPlayState = PLAY_STATE.movingToTable;
+    };
 
-        break;
-      default:
-        break;
-    }
+    var renderPlayerHand = function (nPlayer, aPlayerCards) {
 
-    renderPlayerTable(1, aPlayer1Table);
-    renderPlayerTable(2, aPlayer2Table);
-    renderPlayerHand(1, aPlayer1Cards);
-    renderPlayerHand(2, aPlayer2Cards);
-  };
+        var oPlayerHandView = document.getElementById('hand' + nPlayer);
 
-  var oPlayBtn = document.createElement('button');
-  var oContent = document.createTextNode('Play');
-  oPlayBtn.appendChild(oContent);
-  oPlayBtn.onclick = doTurn.bind(this, aPlayer1Cards, aPlayer2Cards);
-  document.body.insertBefore(oPlayBtn, null);
+        while (oPlayerHandView.firstChild) {
+            oPlayerHandView.removeChild(oPlayerHandView.firstChild);
+        }
 
-  var oPlayer1View = document.createElement('div');
-  oPlayer1View.setAttribute('class', 'player');
-  oPlayer1View.setAttribute('id', 'player1');
+        for (i = 0; i < aPlayerCards.length; i++) {
+            addCardToView(oPlayerHandView, aPlayerCards[i]);
+        }
+    };
 
-  document.body.insertBefore(oPlayer1View, null);
+    var putCardOnTable = function (aPlayerTable, aPlayerCards) {
+        aPlayerTable.push(aPlayerCards[0]);
+        aPlayerCards.splice(0, 1);
+    };
 
-  var oPlayer2View = document.createElement('div');
-  oPlayer2View.setAttribute('class', 'player');
-  oPlayer2View.setAttribute('id', 'player2');
+    var clearTable = function (aPlayerTable) {
+        aPlayerTable.splice(0);
+    };
 
-  document.body.insertBefore(oPlayer2View, null);
+    var getTableCard = function (aPlayerTable) {
+        return aPlayerTable[aPlayerTable.length - 1];
+    };
 
-  var oPlayer1TableView = document.createElement('div');
-  oPlayer1TableView.setAttribute('class', 'table');
-  oPlayer1TableView.setAttribute('id', 'table1');
+    var isGameFinished = function (aPlayer1Cards, aPlayer2Cards) {
+        if (aPlayer1Cards.length === 0) {
+            console.log('player 2 wins');
+            return true;
+        } else if (aPlayer2Cards.length === 0) {
+            console.log('player 1 wins');
+            return true;
+        }
+        return false;
+    };
 
-  oPlayer1View.insertBefore(oPlayer1TableView, null);
+    var makeView = function (aPlayer1Cards, aPlayer2Cards) {
 
-  var oPlayer2TableView = document.createElement('div');
-  oPlayer2TableView.setAttribute('class', 'table');
-  oPlayer2TableView.setAttribute('id', 'table2');
+        var nPlayState = 0;
+        var aPlayer1Table = [];
+        var aPlayer2Table = [];
 
-  oPlayer2View.insertBefore(oPlayer2TableView, null);
+        var PLAY_STATE = {
+            movingToTable: 0,
+            checkingTable: 1
+        }
 
-  var oPlayer1HandView = document.createElement('div');
-  oPlayer1HandView.setAttribute('class', 'hand');
-  oPlayer1HandView.setAttribute('id', 'hand1');
+        var doTurn = function (aPlayer1Cards, aPlayer2Cards) {
 
-  oPlayer1View.insertBefore(oPlayer1HandView, null);
+        switch (nPlayState) {
+            case PLAY_STATE.movingToTable:
 
-  var oPlayer2HandView = document.createElement('div');
-  oPlayer2HandView.setAttribute('class', 'hand');
-  oPlayer2HandView.setAttribute('id', 'hand2');
+                if (isGameFinished(aPlayer1Cards, aPlayer2Cards)) {
+                    return;
+                }
 
-  oPlayer2View.insertBefore(oPlayer2HandView, null);
+                putCardOnTable(aPlayer1Table, aPlayer1Cards);
+                putCardOnTable(aPlayer2Table, aPlayer2Cards);
 
-  renderPlayerTable(1, aPlayer1Table);
-  renderPlayerTable(2, aPlayer2Table);
-  renderPlayerHand(1, aPlayer1Cards);
-  renderPlayerHand(2, aPlayer2Cards);
+                nPlayState = PLAY_STATE.checkingTable;
 
-}
+                break;
 
-var addCardToView = function (oView, nCard) {
+            case PLAY_STATE.checkingTable:
 
-  var oCardView = document.createElement('div');
-  oCardView.setAttribute('class', 'card');
-  oCardView.setAttribute('id', 'card' + nCard);
+                if (getTableCard(aPlayer1Table) > getTableCard(aPlayer2Table)) {
+                    Array.prototype.push.apply(aPlayer1Cards, aPlayer1Table);
+                    Array.prototype.push.apply(aPlayer1Cards, aPlayer2Table);
+                    clearTable(aPlayer1Table);
+                    clearTable(aPlayer2Table);
+                } else if (getTableCard(aPlayer1Table) < getTableCard(aPlayer2Table)) {
+                    Array.prototype.push.apply(aPlayer2Cards, aPlayer1Table);
+                    Array.prototype.push.apply(aPlayer2Cards, aPlayer2Table);
+                    clearTable(aPlayer1Table);
+                    clearTable(aPlayer2Table);
+                } else if (getTableCard(aPlayer1Table) === getTableCard(aPlayer2Table)) {
+                    putCardOnTable(aPlayer1Table, aPlayer1Cards);
+                    putCardOnTable(aPlayer2Table, aPlayer2Cards);
+                }
 
-  var oCardFaceView = document.createElement('div');
-  oCardFaceView.setAttribute('class', 'content');
+                isGameFinished(aPlayer1Cards, aPlayer2Cards);
+                nPlayState = PLAY_STATE.movingToTable;
 
-  var oCardFaceText = document.createTextNode(nCard);
-  oCardFaceView.appendChild(oCardFaceText);
+                break;
+            default:
+                break;
+            }
 
-  oCardView.insertBefore(oCardFaceView, null);
+            renderPlayerTable(1, aPlayer1Table);
+            renderPlayerTable(2, aPlayer2Table);
+            renderPlayerHand(1, aPlayer1Cards);
+            renderPlayerHand(2, aPlayer2Cards);
+        };
 
-  oView.insertBefore(oCardView, null);
-}
+        var oPlayBtn = document.createElement('button');
+        var oContent = document.createTextNode('Play');
+        oPlayBtn.appendChild(oContent);
+        oPlayBtn.onclick = doTurn.bind(this, aPlayer1Cards, aPlayer2Cards);
+        document.body.insertBefore(oPlayBtn, null);
 
-var distribute = function (aCards) {
+        var oPlayer1View = document.createElement('div');
+        oPlayer1View.setAttribute('class', 'player');
+        oPlayer1View.setAttribute('id', 'player1');
 
-  var oGameView = document.getElementById('game');
+        document.body.insertBefore(oPlayer1View, null);
 
-  var i, oCard;
-  var aPlayer1Cards = [],
-      aPlayer2Cards = [];
+        var oPlayer2View = document.createElement('div');
+        oPlayer2View.setAttribute('class', 'player');
+        oPlayer2View.setAttribute('id', 'player2');
 
-  var aDistributedCards = [];
+        document.body.insertBefore(oPlayer2View, null);
 
-  for (i = 0; i < aCards.length; i++) {
-    oCard = aCards[i];
+        var oPlayer1TableView = document.createElement('div');
+        oPlayer1TableView.setAttribute('class', 'table');
+        oPlayer1TableView.setAttribute('id', 'table1');
 
-    if (i % 2 === 0) {
-      aPlayer1Cards.push(oCard);
-    } else {
-      aPlayer2Cards.push(oCard);
-    }
-  }
+        oPlayer1View.insertBefore(oPlayer1TableView, null);
 
-  aDistributedCards[0] = aPlayer1Cards;
-  aDistributedCards[1] = aPlayer2Cards;
+        var oPlayer2TableView = document.createElement('div');
+        oPlayer2TableView.setAttribute('class', 'table');
+        oPlayer2TableView.setAttribute('id', 'table2');
 
-  return aDistributedCards;
+        oPlayer2View.insertBefore(oPlayer2TableView, null);
 
-}
+        var oPlayer1HandView = document.createElement('div');
+        oPlayer1HandView.setAttribute('class', 'hand');
+        oPlayer1HandView.setAttribute('id', 'hand1');
 
-setup();
+        oPlayer1View.insertBefore(oPlayer1HandView, null);
 
-aCards = [1, 4, 6, 5, 3, 1, 2, 6, 6, 1, 4];
+        var oPlayer2HandView = document.createElement('div');
+        oPlayer2HandView.setAttribute('class', 'hand');
+        oPlayer2HandView.setAttribute('id', 'hand2');
 
-aDistributedCards = distribute(aCards);
-makeView(aDistributedCards[0], aDistributedCards[1]);
+        oPlayer2View.insertBefore(oPlayer2HandView, null);
 
-close();
+        renderPlayerTable(1, aPlayer1Table);
+        renderPlayerTable(2, aPlayer2Table);
+        renderPlayerHand(1, aPlayer1Cards);
+        renderPlayerHand(2, aPlayer2Cards);
+    };
+
+    var addCardToView = function (oView, nCard) {
+
+        var oCardView = document.createElement('div');
+        oCardView.setAttribute('class', 'card');
+        oCardView.setAttribute('id', 'card' + nCard);
+
+        var oCardFaceView = document.createElement('div');
+        oCardFaceView.setAttribute('class', 'content');
+
+        var oCardFaceText = document.createTextNode(nCard);
+        oCardFaceView.appendChild(oCardFaceText);
+
+        oCardView.insertBefore(oCardFaceView, null);
+
+        oView.insertBefore(oCardView, null);
+    };
+
+    var distribute = function (aCards) {
+
+        var oGameView = document.getElementById('game');
+
+        var i, oCard;
+        var aPlayer1Cards = [],
+            aPlayer2Cards = [];
+
+        var aDistributedCards = [];
+
+        for (i = 0; i < aCards.length; i++) {
+            oCard = aCards[i];
+
+            if (i % 2 === 0) {
+                aPlayer1Cards.push(oCard);
+            } else {
+                aPlayer2Cards.push(oCard);
+            }
+        }
+
+        aDistributedCards[0] = aPlayer1Cards;
+        aDistributedCards[1] = aPlayer2Cards;
+
+        return aDistributedCards;
+    };
+
+    setup();
+
+    aCards = [1, 4, 6, 5, 3, 1, 2, 6, 6, 1, 4];
+
+    aDistributedCards = distribute(aCards);
+    makeView(aDistributedCards[0], aDistributedCards[1]);
+
+    close();
+} ());
