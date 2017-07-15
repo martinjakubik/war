ed59a31f1797f6a86ae341d22925d846ff8dc23f
martin jakubik <martin.jakubik@gmail.com>
2017-07-15 22:01:15 +0200

    committed Gruntfile with ftp options not in git

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+/*global module */
+/*global require */
+module.exports = function (grunt) {
+    'use strict';
+
+    require('load-grunt-tasks')(grunt);
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    grunt.loadNpmTasks('grunt-dev-update');
+
+    grunt.initConfig({
+        devUpdate: {
+            main: {
+                options: {
+                    updateType: 'prompt'
+                }
+            }
+        },
+        copy: {
+            main: {
+                files: [{
+                    expand: true,
+                    flatten: true,
+                    src: ['node_modules/flook/js/*.js'],
+                    dest: 'js/lib/flook/js/',
+                    filter: 'isFile'
+                },
+                {
+                    expand: true,
+                    flatten: true,
+                    src: ['node_modules/flook/resources/*.wav'],
+                    dest: 'js/lib/flook/resources/',
+                    filter: 'isFile'
+                }],
+            },
+        }
+    });
+
+    // loads ftp config
+    grunt.loadTasks('grunttasks');
+
+    grunt.registerTask('default', [
+        'devUpdate',
+        'copy',
+        'ftpPut'
+    ]);
+};
