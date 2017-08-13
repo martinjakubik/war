d35d0b78269dff1af49b92da67613c8f584aa50f
martin jakubik <martin.jakubik@gmail.com>
2017-08-13 20:10:11 +0200

    moved isGameFinished to kierki

--- js/app/WarGamePlay.js
+++ js/app/WarGamePlay.js
@@ -13,54 +13,29 @@ define('WarGamePlay', ['GamePlay', 'Player', 'Tools', 'GameSession'], function (
     WarGamePlay.prototype = Object.create(GamePlay.prototype);
 
     /**
-     * Indicates who won the hand, based on the two players' cards.
+     * Indicates who has the winnig card on the table, based on the two players'
+     * cards.
      *
-     * @param aPlayerControllers an array of players
+     * @param aPlayerControllers a list of players
      *
-     * @return number of the player who won, or -1 if it's a tie
+     * @return number of the player whose table card is winning, or -1 if it's a
+     *          tie
      */
-    WarGamePlay.prototype.whoWonTheHand = function (aPlayerControllers) {
+    WarGamePlay.prototype.whoseCardWins = function (aPlayerControllers) {
 
         var nWinningPlayer = -1;
 
-        if (aPlayerControllers[0].getTableCard().value > this.playerControllers[1].getTableCard().value) {
-            nWinningPlayer = 0;
-        } else if (aPlayerControllers[0].getTableCard().value < aPlayerControllers[1].getTableCard().value) {
-            nWinningPlayer = 1;
+        if (aPlayerControllers[0].getTable().length > 0
+            && aPlayerControllers[1].getTable().length > 0) {
+
+            if (aPlayerControllers[0].getTableCard().value > this.playerControllers[1].getTableCard().value) {
+                nWinningPlayer = 0;
+            } else if (aPlayerControllers[0].getTableCard().value < aPlayerControllers[1].getTableCard().value) {
+                nWinningPlayer = 1;
+            }
         }
 
         return nWinningPlayer;
-    };
-
-    /**
-     * checks if one player has won
-     */
-    WarGamePlay.prototype.isGameFinished = function () {
-
-        var i, nOtherPlayer;
-
-        for (i = 0; i < this.playerControllers.length; i++) {
-
-            // checks if the player's hand is empty
-            if (this.playerControllers[i].hand.length === 0) {
-
-                if (i === 0) {
-                    nOtherPlayer = 1;
-                } else if (i === 1) {
-                    nOtherPlayer = 0;
-                }
-
-                // checks if the same player's table loses to the other player
-                if (this.playerControllers[i].getTableCard().value < this.playerControllers[nOtherPlayer].getTableCard().value) {
-
-                    this.result = this.playerControllers[nOtherPlayer].getName() + ' wins';
-                    this.callbacks.renderResult(this.result);
-                    this.state = GAME_OVER;
-                    return true;
-                }
-            }
-        }
-        return false;
     };
 
     /**
