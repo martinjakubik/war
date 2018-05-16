db548a216c033d04745d51ea0faf2e064ca9128f
martin jakubik <martin.jakubik@gmail.com>
2018-05-16 21:52:05 +0200

    swift: updated playground

--- batanimal/MyPlayground.playground/Contents.swift
+++ batanimal/MyPlayground.playground/Contents.swift
@@ -2,4 +2,21 @@
 
 import UIKit
 
-var str = "Hello, playground"
+let maxNumberOfGameSlots = 3
+
+let firstSlot = 3
+
+var currentSlot = firstSlot
+var increment = 0
+
+var counter = 0
+
+while counter < 10 {
+
+    print(currentSlot)
+    
+    increment = (increment + 1) % maxNumberOfGameSlots
+    currentSlot = firstSlot + increment
+
+    counter = counter + 1
+}
