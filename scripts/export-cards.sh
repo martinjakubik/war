7ba3d07a250897bbbf202de802acdc4cc270351d
martin jakubik <martin.jakubik@sap.com>
2025-02-15 13:31:56 +0100

    pointed script to resource sources

--- scripts/export-cards.sh
+++ scripts/export-cards.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-inkscape resources/card-batanimo.svg -i layer38 -j -C --export-png=build/border.png
-inkscape resources/card-batanimo.svg -i layer39 -j -C --export-png=build/shadow.png
-inkscape resources/card-batanimo.svg -i layer41 -j -C --export-png=build/measure.forhtml.png --export-area=11:20:228:358
-inkscape resources/card-batanimo.svg -i layer41 -j -C --export-png=build/measure.forswift.png
+inkscape resources-src/card-batanimo.svg -i layer38 -j -C --export-png=build/border.png
+inkscape resources-src/card-batanimo.svg -i layer39 -j -C --export-png=build/shadow.png
+inkscape resources-src/card-batanimo.svg -i layer41 -j -C --export-png=build/measure.forhtml.png --export-area=11:20:228:358
+inkscape resources-src/card-batanimo.svg -i layer41 -j -C --export-png=build/measure.forswift.png
 
 # regular cards
 for layerId in {1..36}; do
-    label=$(xmllint --xpath "string(/*[local-name() = 'svg']/*[local-name() = 'g'][@*[local-name() = 'id'] = 'layer${layerId}']/@*[local-name() = 'label'])"  resources/card-batanimo.svg)
+    label=$(xmllint --xpath "string(/*[local-name() = 'svg']/*[local-name() = 'g'][@*[local-name() = 'id'] = 'layer${layerId}']/@*[local-name() = 'label'])"  resources-src/card-batanimo.svg)
 
     # exports two versions of the drawing scene
-    inkscape resources/card-batanimo.svg -i layer${layerId} -j -C --export-png=build/card${label}.scene.forswift.png 
-    inkscape resources/card-batanimo.svg -i layer${layerId} -j -C --export-png=build/card${label}.scene.forhtml.png --export-area=11:20:228:358
+    inkscape resources-src/card-batanimo.svg -i layer${layerId} -j -C --export-png=build/card${label}.scene.forswift.png 
+    inkscape resources-src/card-batanimo.svg -i layer${layerId} -j -C --export-png=build/card${label}.scene.forhtml.png --export-area=11:20:228:358
 
     # adds measurement layer on top for html version
     convert -background none -page +0+0 build/card${label}.scene.forhtml.png -page +0+0 build/measure.forhtml.png -layers merge +repage build/card-batanimo-${label}.png
@@ -25,11 +25,11 @@ done
 
 # skunk card
 for layerId in {37..37}; do
-    label=$(xmllint --xpath "string(/*[local-name() = 'svg']/*[local-name() = 'g'][@*[local-name() = 'id'] = 'layer${layerId}']/@*[local-name() = 'label'])"  resources/card-batanimo.svg)
+    label=$(xmllint --xpath "string(/*[local-name() = 'svg']/*[local-name() = 'g'][@*[local-name() = 'id'] = 'layer${layerId}']/@*[local-name() = 'label'])"  resources-src/card-batanimo.svg)
 
     # exports two versions of the drawing scene
-    inkscape resources/card-batanimo.svg -i layer${layerId} -j -C --export-png=build/card-batanimo-${label}.forswift.png
-    inkscape resources/card-batanimo.svg -i layer${layerId} -j -C --export-png=build/card-batanimo-${label}.png --export-area=11:20:228:358
+    inkscape resources-src/card-batanimo.svg -i layer${layerId} -j -C --export-png=build/card-batanimo-${label}.forswift.png
+    inkscape resources-src/card-batanimo.svg -i layer${layerId} -j -C --export-png=build/card-batanimo-${label}.png --export-area=11:20:228:358
 
     # adds border and shadow to swift version
     convert -background none -page +0+0 build/shadow.png -page +0+0 build/border.png -page +0+0 build/card-batanimo-${label}.forswift.png -layers merge +repage build/card-batanimo-${label}-bo-sh.png
